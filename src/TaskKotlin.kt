8b27631a0e49b3867f196dd809b0b990222782ea
AdamDz <71586194+AbBlDz@users.noreply.github.com>
2020-10-11 17:27:26 +0700

    Убрал лишний импорт и println
    Убрал импорт, который не использовался.
    Убрал println, который мешался при выводе тестов.

--- src/TaskKotlin.kt
+++ src/TaskKotlin.kt
@@ -1,5 +1,3 @@
-import java.util.*
-
 fun main(args: Array<String>) {
 
     // Проверяю были ли получены данные и если данные отсутствуют, принимаю их со стандартного потока ввода
@@ -15,11 +13,11 @@ fun main(args: Array<String>) {
         uniqueList.add(element)
     }
 
-    var wordsList: MutableMap<String, Int> = mutableMapOf()
+    val wordsList: MutableMap<String, Int> = mutableMapOf()
     var countWords: Int
 
     for (uniqueElement in uniqueList) {
-        countWords = 0;
+        countWords = 0
         for (element in wordsArray) {
             if (uniqueElement == element) {
                 countWords++
@@ -30,8 +28,7 @@ fun main(args: Array<String>) {
 
     wordsList.toList().sortedByDescending { it.second }.toMap()
 
-    println("\n Output the result: \n")
     wordsList.forEach { element ->
-        println(" ${element.key} ${element.value}")
+        println(" ${element.key} - ${element.value}")
     }
 }
