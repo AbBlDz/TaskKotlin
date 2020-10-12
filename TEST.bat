bde0a7681f4b8b9b9f3925fed860db902d3592ba
AdamDz <71586194+AbBlDz@users.noreply.github.com>
2020-10-13 00:40:45 +0700

    #1 9. Добавил тесты

--- TEST.bat
+++ TEST.bat
@@ -12,7 +12,7 @@ echo  --------------------------------------------------------
 echo  Test 1. Data is passed to the program.
 echo  --------------------------------------------------------
 echo  Data passed in the program: the the quick brown fox jumps
-echo  Expected: the - 2 quick - 1 brown - 1 fox - 1 jumps - 1
+echo  Expected: the - 2 brown - 1 fox - 1 jumps - 1 quick - 1
 echo  Result of running:
 java -jar TaskKotlin.jar the the quick brown fox jumps
 
@@ -23,7 +23,7 @@ echo  ---------------------------------------------------------
 echo  Test 2. Data is received via a standard stream.
 echo  ---------------------------------------------------------
 echo  Data passed in the program: the the quick brown fox jumps
-echo  Expected: the - 2 quick - 1 brown - 1 fox - 1 jumps - 1
+echo  Expected: the - 2 brown - 1 fox - 1 jumps - 1 quick - 1
 echo  Result of running:
 echo the the quick brown fox jumps| java -jar TaskKotlin.jar
 
@@ -33,22 +33,55 @@ echo.
 echo  ---------------------------------------------------------
 echo  Test 3. The same words are passed to the program
 echo  ---------------------------------------------------------
-echo  Data passed in the program: the the the the the the
-echo  Expected: the - 6
+echo  Data passed in the program: the the the the the the the the the the the the the the the the the the the the
+echo  Expected: the - 20
 echo  Result of running:
-java -jar TaskKotlin.jar the the the the the the
+java -jar TaskKotlin.jar the the the the the the the the the the the the the the the the the the the the
 
 
 :: TEST 4
 
 echo.
 echo  ---------------------------------------------------------
-echo  Test 4. Passing a single word to the program
+echo  Test 4. Special characters are passed to the program
 echo  ---------------------------------------------------------
-echo  Data passed in the program: the
-echo  Expected: the - 1
+echo  Data passed in the program: ★★★
+echo  Expected: ★★★ - 1
 echo  Result of running:
-java -jar TaskKotlin.jar the
+java -jar TaskKotlin.jar ★★★
 echo.
 
+
+:: TEST 5
+
+echo.
+echo  ---------------------------------------------------------
+echo  Test 5. An empty string was passed to the program
+echo  ---------------------------------------------------------
+echo  Data passed in the program:
+echo  Expected: Waiting for a line to be entered
+echo  Result of running:
+java -jar TaskKotlin.jar
+
+:: TEST 6
+
+echo.
+echo  ---------------------------------------------------------
+echo  Test 6. Several spaces were passed to the program
+echo  ---------------------------------------------------------
+echo  Data passed in the program:       
+echo  Expected: (ECHO) - 1  - 1 Є ¤ - 1 нЄа  - 1  Ё - 1 ўЄоз. - 1 ўлў¤  - 1
+echo  Result of running:
+echo       | java -jar TaskKotlin.jar
+
+:: TEST 7
+
+echo.
+echo  ---------------------------------------------------------
+echo  Test 7. The program is passed a string with several spaces between words
+echo  ---------------------------------------------------------
+echo  Data passed in the program: the        the   
+echo  Expected:  - 7 the - 2
+echo  Result of running:
+echo the        the| java -jar TaskKotlin.jar 
 pause
